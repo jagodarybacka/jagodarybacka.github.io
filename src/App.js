8aa1ee0d8e63e67e76032839c6bb15b1cb0eb983
jagodarybacka <jagoda.roza.rybacka@gmail.com>
2020-12-25 20:03:58 +0100

    Preparing new portfolio version

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App__circle">
+        <p>Hi. My name is Berry. I develop web applications and I teach Javascript</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
