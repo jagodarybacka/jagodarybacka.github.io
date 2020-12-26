a465662db328ff2f591ec2d48ad13035c4984473
jagodarybacka <jagoda.roza.rybacka@gmail.com>
2020-12-26 17:29:08 +0100

    feat: Changing background with route

--- src/App.js
+++ src/App.js
@@ -1,13 +1,34 @@
-import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import { Switch, Route, useLocation } from 'react-router-dom';
 import HomePage from 'pages/Home';
 import DeveloperPage from 'pages/Developer';
 import MentorPage from 'pages/Mentor';
 import './App.scss';
 
+const ROUTES = {
+  MENTOR: '/mentor',
+  DEVELOPER: '/developer'
+};
+
 function App() {
+  const location = useLocation();
+  const [ styles, setStyles ] = useState({});
+
+  useEffect(() => {
+    switch (location.pathname) {
+    case ROUTES.MENTOR:
+      setStyles({ borderColor: '#cb8e7e', backgroundColor: '#000000' });
+      break;
+    case ROUTES.DEVELOPER:
+      setStyles({ borderColor: '#253439', backgroundColor: '#000000' });
+      break;
+    default:
+      setStyles({});
+    }
+  }, [location]);
+
   return (
-    <div className="App">
+    <div className="App" style={styles}>
       <Switch>
         <Route path="/developer">
           <DeveloperPage />
