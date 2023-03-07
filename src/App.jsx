19702eb5899cfb0deadd05929d7732fb20cacbe2
Jagoda Berry Rybacka <berry.rybacka@gmail.com>
2023-03-07 20:13:31 +0100

    Updated dependencies and cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,14 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
 import HomePage from 'pages/Home';
-import Blog from 'pages/Blog';
 import Footer from 'components/Footer';
 import Header from 'components/Header';
-import './App.scss';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Header />
-      <Switch>
-        <Route path="/blog">
-          <Blog />
-        </Route>
-        <Route path="/">
-          <HomePage />
-        </Route>
-      </Switch>
+      <HomePage />
       <Footer />
     </div>
   );
